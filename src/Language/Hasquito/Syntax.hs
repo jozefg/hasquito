62dbfb1a717dafd3e97826c14c589385b84f95e6
Danny Gratzer <jozefg@cmu.edu>
2014-07-28 23:33:13 -0400

    A lot of the AST

--- src/Language/Hasquito/Syntax.hs
+++ src/Language/Hasquito/Syntax.hs
@@ -1,1 +1,40 @@
 module Language.Hasquito.Syntax where
+import Control.Monad.Except
+import Control.Monad.Gen
+
+data Name = Gen Int | Name String
+          deriving(Eq, Show, Ord)
+instance Enum Name where
+  toEnum           = Gen
+  fromEnum (Gen i) = i
+  fromEnum Name{}  = error "Impossible! Attempted to fromEnum a Name"
+
+-- | Errors from some part of the application
+data Error = TCError String
+           deriving Show
+
+type CompilerM = ExceptT Error (Gen Name) 
+
+freshName :: CompilerM Name -- I really need to update monad-gen
+freshName = lift gen
+
+-- | Type of all Hasquito expression
+data Ty = TArr Ty Ty -- ^ Function type
+        | TNum       -- ^ Numeric type
+        | TVar Name  -- ^ Implicitly universally quantified type var
+        deriving (Eq, Show)
+
+data Prim = Plus | Minus | Mult | Div
+          deriving(Eq, Show)
+
+data Exp = Prim Prim
+         | Num Int
+         | App Exp [Exp]
+         | Lam [(Name, Ty)] Exp
+         deriving(Eq, Show)
+
+data Def meta = Def { defTy    :: Ty
+                    , defName  :: Name
+                    , defBody  :: Exp
+                    , defMeta  :: meta }
+              deriving(Eq, Show)
