8239b5229ce029300298aeb2a60fca7e0aa66ec5
Danny Gratzer <jozefg@cmu.edu>
2014-07-29 16:30:14 -0400

    More parsing, less confidence in correctness wrt fixity/recursion

--- src/Language/Hasquito/Parser.hs
+++ src/Language/Hasquito/Parser.hs
@@ -8,6 +8,9 @@ import Language.Hasquito.Syntax
 name :: Parser Name
 name = Name . T.pack <$> many1 letter
 
+tparen :: Parser Ty
+tparen = char '(' *> ty <* char ')'
+
 tarr :: Parser Ty
 tarr = do
   l <- ty
@@ -22,10 +25,42 @@ tvar :: Parser Ty
 tvar = TVar <$> name
 
 ty :: Parser Ty
-ty = skipSpace *> (tvar <|> tnum <|> tarr) <* skipSpace
+ty = skipSpace *> (try tarr <|> tparen <|> tnum <|> tvar) <* skipSpace
 
 num :: Parser Exp
 num = Num <$> signed decimal
 
 var :: Parser Exp
 var = Var <$> name
+
+app :: Parser Exp
+app = do
+  l <- expr
+  char ' ' *> skipSpace
+  r <- expr
+  return (App l r)
+
+bindings :: Parser [(Name, Ty)]
+bindings = many1 binding
+  where binding = do
+          char '(' *> skipSpace 
+          n <- name
+          skipSpace *> char ':'
+          t <- ty
+          return (n, t)
+
+lam :: Parser Exp
+lam = do
+  char '('
+  vars <- bindings
+  skipSpace *> string " -> " *> skipSpace
+  body <- expr
+  char ')'
+  return (Lam vars body)
+
+eparen :: Parser Exp
+eparen = char '(' *> expr <* skipSpace <* char ')'
+
+expr :: Parser Exp
+expr = skipSpace *> (eparen <|> try app <|> try lam <|> var <|> num)
+
