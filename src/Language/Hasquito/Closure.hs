e594d9d11c8dbcafe417cf8f5f34e08fc6f827de
Danny Gratzer <jozefg@cmu.edu>
2014-07-30 02:10:55 -0400

    Added some simple lambda lifting

--- src/Language/Hasquito/Closure.hs
+++ src/Language/Hasquito/Closure.hs
@@ -1,6 +1,9 @@
 module Language.Hasquito.Closure where
+import Control.Applicative
+import Control.Monad.Writer
 import Data.List
 import Language.Hasquito.Syntax
+import Language.Hasquito.Util
 
 freeVars :: Exp -> [Name]
 freeVars Num{}           = []
@@ -17,3 +20,14 @@ closConv (App l r) = closConv l `App` closConv r
 closConv (Lam vars body) =
   let others = freeVars (Lam vars body)
   in Lam (others ++ vars) body
+
+liftLam :: Exp -> WriterT [Def ()] CompilerM Exp
+liftLam (Num i) = return $ Num i
+liftLam (Prim p) = return $ Prim p
+liftLam (Var n) = return $ Var n
+liftLam (App l r) = App <$> liftLam l <*> liftLam r
+liftLam l@(Lam vars body) = do
+  name <- lift freshName
+  tell [Def TNum name l ()]
+  return (Var name)
+
