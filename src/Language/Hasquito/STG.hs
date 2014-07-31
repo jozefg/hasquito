bd222f4cbbcfdc3cd28a1a4b5f058dbae9e407cb
Danny Gratzer <jozefg@cmu.edu>
2014-07-30 22:33:16 -0400

    Added a few more data types to STG

--- src/Language/Hasquito/STG.hs
+++ src/Language/Hasquito/STG.hs
@@ -1,9 +1,23 @@
 module Language.Hasquito.STG where
 import Language.Hasquito.Syntax
 
+-- | The top level that all declarations will be
+-- compiled to.
+data TopLevel = Thunk SExp
+              | Fun [Name] SExp
+
+-- | The new expression language, includes
+-- primitive ints, variables, and full and partial
+-- application.
 data SExp = SNum Int
           | SVar Name
           | SApp SExp [SExp]
           | FullApp Op SExp SExp
-          | App SExp [SExp]
+
+data InfoTable = InfoTable { arity   :: Int
+                           , codePtr :: SExp }
+
+data Closure = Closure { freeVars :: [SExp]
+                       , table    :: InfoTable }
+
 
