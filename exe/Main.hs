63bcc8cf9e139ba0751a058ade8cfbba27057e03
Danny Gratzer <jozefg@cmu.edu>
2014-08-01 16:14:30 -0400

    Main.hs now just runs the compilation pipeline

--- exe/Main.hs
+++ exe/Main.hs
@@ -5,8 +5,4 @@ import System.Environment
 main :: IO ()
 main = do
   [f] <- getArgs
-  decs <- parseFile f
-  let res = decs >>= runCompilerM . typeCheck
-  case res of
-    Left errors -> print errors
-    Right _     -> putStrLn "It typechecks"
+  compileFile f
