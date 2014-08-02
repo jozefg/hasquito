5b5c33dc77e1b67a26bf26ed6ba1db9f30278a83
Danny Gratzer <jozefg@cmu.edu>
2014-08-01 23:17:52 -0400

    Start of an abstract machine to compile STG to

---
+++ src/Language/Hasquito/AbsMachine.hs
@@ -0,0 +1,16 @@
+module Language.Hasquito.AbsMachine where
+import Language.Hasquito.Syntax
+
+data Reg = SpA | SpB | SpC | SpF | Node
+
+data Stmt = WriteStack Reg Int Int -- ^ Push an prim/address onto a stack
+          | ReadStack Name Reg Int -- ^ Assign the Int th argument to Name
+          | AdjustSP Reg Int       -- ^ Increment or decrement a SP
+          | Enter Int              -- ^ Enter a closure at a particular addr
+
+type Program = [Stmt]
+
+
+data InfoTable = Info { entryCode :: Program }
+data Closure = Closure { info   :: InfoTable
+                       , closed :: [Int] }
