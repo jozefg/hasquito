7919ce40608b18589ebe2bbeb4a2a03d757b3757
Danny Gratzer <jozefg@cmu.edu>
2014-08-01 23:50:45 -0400

    Some comments on what exactly each register is

--- src/Language/Hasquito/AbsMachine.hs
+++ src/Language/Hasquito/AbsMachine.hs
@@ -1,7 +1,15 @@
 module Language.Hasquito.AbsMachine where
 import Language.Hasquito.Syntax
 
-data Reg = SpA | SpB | SpC | SpF | BpA | BpB | Node | Ret | User Int
+data Reg = SpA -- ^ stack for ints
+         | SpB -- ^ stack for closures addresses
+         | SpC -- ^ stack for continuations
+         | SpF -- ^ stack for update frames
+         | BpA -- ^ saving stack value of SpA
+         | BpB -- ^ saving stack value of SpB
+         | Node -- ^ Current closure
+         | Ret -- ^ Return register for integers
+         | User Int -- ^ A user defined register
 
 data Stmt = WriteStack Reg Int Int -- ^ Push an prim/address onto a stack
           | ReadStack Name Reg Int -- ^ Assign the Int th argument to Name
