05eb57a7c42dd7b88d8b0246dd72022ae4101046
Danny Gratzer <jozefg@cmu.edu>
2014-08-01 23:46:21 -0400

    Added update frames and allocation instructions

--- src/Language/Hasquito/AbsMachine.hs
+++ src/Language/Hasquito/AbsMachine.hs
@@ -1,12 +1,14 @@
 module Language.Hasquito.AbsMachine where
 import Language.Hasquito.Syntax
 
-data Reg = SpA | SpB | SpC | SpF | Node | User Int
+data Reg = SpA | SpB | SpC | SpF | BpA | BpB | Node | User Int
 
 data Stmt = WriteStack Reg Int Int -- ^ Push an prim/address onto a stack
           | ReadStack Name Reg Int -- ^ Assign the Int th argument to Name
           | AdjustSP Reg Int       -- ^ Increment or decrement a SP
           | Enter Int              -- ^ Enter a closure at a particular addr
+          | AllocC Name Closure    -- ^ Allocate a closure, store result
+          | AllocF Name Frame      -- ^ Allocate a frame, store the result
 
 type Program = [Stmt]
 
@@ -14,3 +16,4 @@ type Program = [Stmt]
 data InfoTable = Info { entryCode :: Program }
 data Closure = Closure { info   :: InfoTable
                        , closed :: [Int] }
+data Frame = Frame {closurePtr :: Int}
