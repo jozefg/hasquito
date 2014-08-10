d3a5922bb4e19af21658737e1ca91bfbc1f4549e
Danny Gratzer <jozefg@cmu.edu>
2014-08-10 07:02:15 -0400

    Main compiler pipeline now pretty prints all the variable bindings

--- src/Language/Hasquito.hs
+++ src/Language/Hasquito.hs
@@ -10,9 +10,16 @@ import Language.Hasquito.STG as H
 import Language.Hasquito.Uniquify as H
 import Language.Hasquito.DeExp as H
 import Control.Monad
+import Language.JavaScript.AST
+import Language.Hasquito.JSify
+import Language.JavaScript.Pretty as J
+import Text.PrettyPrint.Leijen
 
-mainCompiler :: [Def ()] -> CompilerM [TopLevel]
-mainCompiler = typeCheck >=> simplify >=> toSTG >=> deExp >=> uniquify
+mainCompiler :: [Def ()] -> CompilerM [VarDecl]
+mainCompiler = typeCheck >=> simplify >=> toSTG >=> deExp >=> uniquify >=> jsify
+
+prettyPrint :: [VarDecl] -> IO ()
+prettyPrint = print . foldr1 (<>) . map J.pretty
 
 compileFile :: FilePath -> IO ()
 compileFile file = do
@@ -20,4 +27,4 @@ compileFile file = do
   print parseRes
   putStrLn "\n\n\n"
   let compRes = parseRes >>= runCompilerM . mainCompiler
-  either print print compRes
+  either print prettyPrint compRes
