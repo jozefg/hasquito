7d155d4ac84d8c6fe0f30bc696aba052fb1901e6
Danny Gratzer <jozefg@cmu.edu>
2014-08-10 07:23:57 -0400

    Added makeMain to the full compiler pipe line

--- src/Language/Hasquito.hs
+++ src/Language/Hasquito.hs
@@ -1,29 +1,25 @@
 module Language.Hasquito (
   module H,
   compileFile) where
+import Control.Monad
+import Language.Hasquito.Closure as H
+import Language.Hasquito.DeExp as H
+import Language.Hasquito.JSify
+import Language.Hasquito.MakeMain
 import Language.Hasquito.Parser as H
+import Language.Hasquito.STG as H
 import Language.Hasquito.Syntax as H
 import Language.Hasquito.TypeCheck as H
-import Language.Hasquito.Util as H
-import Language.Hasquito.Closure as H
-import Language.Hasquito.STG as H
 import Language.Hasquito.Uniquify as H
-import Language.Hasquito.DeExp as H
-import Control.Monad
+import Language.Hasquito.Util as H
 import Language.JavaScript.AST
-import Language.Hasquito.JSify
 import Language.JavaScript.Pretty as J
-import Text.PrettyPrint.Leijen
 
-mainCompiler :: [Def ()] -> CompilerM [VarDecl]
-mainCompiler = typeCheck >=> simplify >=> toSTG >=> deExp >=> uniquify >=> jsify
-
-prettyPrint :: [VarDecl] -> IO ()
-prettyPrint = print . foldr1 smush . map J.pretty
-  where smush a b = a <> line <> b
+mainCompiler :: [Def ()] -> CompilerM Program
+mainCompiler = typeCheck >=> simplify >=> toSTG >=> deExp >=> uniquify >=> jsify >=> makeMain
 
 compileFile :: FilePath -> IO ()
 compileFile file = do
   parseRes <- parseFile file
   let compRes = parseRes >>= runCompilerM . mainCompiler
-  either print prettyPrint compRes
+  either print (print . J.pretty) compRes
