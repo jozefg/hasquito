38428c7a62de1fdc04d840d90439143da53c4c43
Danny Gratzer <jozefg@cmu.edu>
2014-08-18 17:12:09 -0400

    Added IfZ rules to simplification

--- src/Language/Hasquito/Closure.hs
+++ src/Language/Hasquito/Closure.hs
@@ -10,6 +10,7 @@ freeVars Num{}            = S.empty
 freeVars Op{}             = S.empty
 freeVars (Var n)          = S.singleton n
 freeVars (App l r)        = freeVars l `S.union` freeVars r
+freeVars (IfZ n l r)      = freeVars n `S.union` freeVars l `S.union` freeVars r
 freeVars (Lam _ var body) = S.delete var (freeVars body)
 
 saturate :: Exp -> CompilerM Exp
@@ -17,6 +18,7 @@ saturate (Op o) = do
   [l, r] <- sequence [freshName, freshName]
   return $ Lam [] l $ Lam [] r (App (App (Op o) (Var l)) $ Var r)
 saturate (App l r) = App <$> saturate l <*> saturate r
+saturate (IfZ n l r) = IfZ <$> saturate n <*> saturate l <*> saturate r
 saturate (Lam cs vs body) = Lam cs vs <$> saturate body
 saturate e = return e
 
@@ -25,6 +27,7 @@ closConv (Num i) = Num i
 closConv (Op p) = Op p
 closConv (Var n) = Var n
 closConv (App l r) = closConv l `App` closConv r
+closConv (IfZ n l r) = IfZ (closConv n) (closConv l) (closConv r)
 closConv (Lam _ vars body) =
   let others = S.toList (freeVars (Lam [] vars body))
   in Lam others vars (closConv body)
@@ -34,6 +37,7 @@ liftLam (Num i) = return $ Num i
 liftLam (Op p) = return $ Op p
 liftLam (Var n) = return $ Var n
 liftLam (App l r) = App <$> liftLam l <*> liftLam r
+liftLam (IfZ n l r) = IfZ <$> liftLam n <*> liftLam l <*> liftLam r
 liftLam (Lam closed var body) = do
   body' <- liftLam body
   name <- lift freshName
