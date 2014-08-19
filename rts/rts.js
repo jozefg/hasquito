dadaa3a49f94ccafaa9424861eeb091359748a5c
Danny Gratzer <jozefg@cmu.edu>
2014-08-19 15:28:55 -0400

    Reshuffled rts.js for some organization

--- rts/rts.js
+++ rts/rts.js
@@ -1,31 +1,28 @@
-/* A global variable that represents the current
-   closure we're evaluating */
+/* Global variables */
+// A global variable that represents the curren closure we're evaluating
 var NODE = undefined
 
-/* Another global variable with the current argument stack */
+// Another global variable with the current argument stack
 var ARG_STACK  = []
 
-/* Another stack with all the current evaluated primitives */
+// Another stack with all the current evaluated primitives
 var EVAL_STACK = []
 
-/* Stack for continuations */
+// Stack for continuations
 var CONT_STACK = []
 
+/* Helpers for CodeGen, purely for simplicity */
 var mkClosure = function(entry_code, closed, update){
     return {entry : entry_code, closed_vars : closed, shouldUpdate : update}
-}
-
-var primReturn = function(num){
-    return function(){
-        EVAL_STACK.push(num);
-        return CONT_STACK.pop();
-    }
 }
 
 var doUpdate = function (closure) {
     return function(){
         var result = EVAL_STACK[0]; // Notice that we don't pop this
-        closure.entry = primReturn(result);
+        closure.entry = function() {
+            EVAL_STACK.push(result);
+            return CONT_STACK.pop();
+        };
         return jumpNext()(); // Continue on with the computation
     };
 }
@@ -36,27 +33,6 @@ var enter = function(closure){
         CONT_STACK.push(doUpdate(closure));
     }
     return closure.entry;
-}
-
-var enterMain = function(){
-    var clos = mkClosure(main, [], 1);
-    CONT_STACK.push(terminal);
-    mainLoop(clos);
-}
-
-var jumpNext = function(){
-    var next = CONT_STACK.pop();
-    return next;
-}
-
-var nextArg = function(){
-    return ARG_STACK.pop();
-}
-
-var evalFirst = function(){
-    var closure = ARG_STACK.pop();
-    NODE = closure;
-    return enter(closure);
 }
 
 var sif = function(n, l, r){
@@ -73,42 +49,70 @@ var sif = function(n, l, r){
         }
     });
     return enter(n);
+};
+
+var jumpNext = function(){
+    var next = CONT_STACK.pop();
+    return next;
 }
 
-var doPlus = function(){
-    right = EVAL_STACK.pop();
-    left  = EVAL_STACK.pop();
-    EVAL_STACK.push(left + right);
-    return jumpNext();
+var nextArg = function(){
+    return ARG_STACK.pop();
 }
-var doMinus = function(){
-    right = EVAL_STACK.pop();
-    left  = EVAL_STACK.pop();
-    EVAL_STACK.push(left - right);
-    return jumpNext();
-}
-var doMult = function(){
-    right = EVAL_STACK.pop();
-    left  = EVAL_STACK.pop();
-    EVAL_STACK.push(left * right);
-    return jumpNext();
-}
-var doDiv = function(){
-    right = EVAL_STACK.pop();
-    left  = EVAL_STACK.pop();
-    EVAL_STACK.push(left / right);
-    return jumpNext();
-}
+
+/* Continuations */
 
 var terminal = function(){
     console.log(EVAL_STACK.pop());
     return 0;
 }
 
-var mainLoop = function(clos){
+
+var evalFirst = function(){
+    var closure = ARG_STACK.pop();
+    NODE = closure;
+    return enter(closure);
+}
+
+/* Primitives */
+var doPlus = function(){
+    right = EVAL_STACK.pop();
+    left  = EVAL_STACK.pop();
+    EVAL_STACK.push(left + right);
+    return jumpNext();
+};
+var doMinus = function(){
+    right = EVAL_STACK.pop();
+    left  = EVAL_STACK.pop();
+    EVAL_STACK.push(left - right);
+    return jumpNext();
+};
+var doMult = function(){
+    right = EVAL_STACK.pop();
+    left  = EVAL_STACK.pop();
+    EVAL_STACK.push(left * right);
+    return jumpNext();
+};
+var doDiv = function(){
+    right = EVAL_STACK.pop();
+    left  = EVAL_STACK.pop();
+    EVAL_STACK.push(left / right);
+    return jumpNext();
+};
+
+/* Running Main */
+
+var trampoline = function(clos){
     var cont = function(){ return enter(clos) };
     while(cont){
         cont = cont();
     }
 }
-    
+
+var enterMain = function(){
+    var clos = mkClosure(main, [], 1);
+    CONT_STACK.push(terminal);
+    trampoline(clos);
+}
+
+
